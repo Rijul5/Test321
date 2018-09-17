00d567131172307d9a721549587564add81b0e32
Rijul Saini <rijulsaini.mait@gmail.com>
2018-09-16 23:22:39 -0400

    hello

---
+++ src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.studentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+}
